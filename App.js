8cceedf2d222d0e202054cb9a49c66599a42dea6
vedant prasad priya karle <rays57529@gmail.com>
2022-01-01 21:27:53 +0530

    custom text input created

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-import AccountScreen from "./app/screens/AccountScreen";
+import AppTextInput from "./app/components/AppTextInput";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-	return <AccountScreen />;
+	return (
+		<Screen>
+			<AppTextInput placeholder='Username' icon='email' />
+		</Screen>
+	);
 }
